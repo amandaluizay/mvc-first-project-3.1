b49f0f7fa0fa3f16ba5716d1bee65107badcbf4c
amanda <luiza.amandasilvah@gmail.com>
2022-07-27 16:56:43 -0300

    27.07

---
+++ src/dev.appModelo/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace dev.appModelo.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/dev.appModelo/Extensions/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace dev.appModelo.Extension
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "desenvolvedor.io";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
